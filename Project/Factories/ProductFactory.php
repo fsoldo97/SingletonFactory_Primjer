7c159efb79cef0d4e0337cf7a362083012f7f322
Filip <filip@saguaro.hr>
2025-11-26 18:59:21 +0100

    dodana logika za factory klasu

---
+++ Project/Factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Project\Factories;
+
+
+use Project\Models\Category;
+use Project\Models\Product;
+
+class ProductFactory{
+
+    public static function create(
+        string $name,
+        float $price,
+        string $catName,
+        string $catDesc
+    ): Product{
+        $category = new Category($catName,$catDesc);
+        echo "<br>Factory: Kreirana kategorija '{$category->name}'";
+        $product = new \Product($name,$price,$category);
+        echo "<br> Factory: Kreiran Proizvod '{$product->name}'";
+        return $product;
+    }
+}
+
+
+?>
